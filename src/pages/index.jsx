597cf8a313324fa7551406a69d27042b72febc0e
connor101kendall <connor101kendall@gmail.com>
2023-02-01 19:52:11 -0500

    Intial package fix

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,8 @@
 import React, {useState} from 'react'
 import Footer from '../components/Footer';
 import Hero from '../components/Hero'
-import About from '../components/About'
-import Experience from '../components/Experience'
+import About from '../components/about'
+import Experience from '../components/experience'
 import Navbar from '../components/Navbar'
 import Projects from '../components/Projects';
 import Sidebar from '../components/SideBar'
